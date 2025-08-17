d6a87eb108ca9c6eeb2677fc69d0844d19d068ed
cloudstacker <iamjayakrishna7@gmail.com>
2025-08-17 12:22:29 +0530

    Java App: Fixed syntax issue in main.java

--- java-app/src/main/java/com/example/Main.java
+++ java-app/src/main/java/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Java + Maven!");
-        System.out.println("Testing CI")
+        System.out.println("Testing CI");
     }
 }
 
